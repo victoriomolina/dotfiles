e46c9bc190c5c816aef5d8583ec074fbe5f10d0c
D3us <victoriomolinabermejo@gmail.com>
2025-09-24 20:50:16 +0200

    feat(snacks, nvim): Enable hidden and ignored files in explorer

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true, -- hidden files
+        ignored = true, -- .gitignore files
+      },
+    },
+  },
+}
