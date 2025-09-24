c7fe71402ed905d986ebe53b1c9b35b0508dec00
D3us <victoriomolinabermejo@gmail.com>
2025-09-24 02:47:36 +0200

    fix(keymap, nvim): Solve deprecated issue with diagnostics bind

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -41,5 +41,5 @@ keymap.set("n", "<C-w><down>", "<C-w>-")
 
 -- Diagnostics
 keymap.set("n", "<C-j>", function()
-  vim.diagnostic.goto_next()
+  vim.diagnostic.jump({ count = 1, float = true })
 end, opts)
