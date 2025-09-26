7e9acb051e54f8f951955f9eab01812a2a38fecd
D3us <victoriomolinabermejo@gmail.com>
2025-09-26 12:39:37 +0200

    feat(incline, nvim): Setup incline for better experience with tabs and filenames

---
+++ nvim/.config/nvim/lua/plugins/incline.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "b0o/incline.nvim",
+    event = "BufReadPre", -- Load this plugin before reading a buffer
+    priority = 1200, -- Set the priority for loading this plugin
+    opts = function()
+      require("incline").setup({
+        -- Window margin
+        window = {
+          margin = {
+            vertical = 0,
+            horizontal = 1,
+          },
+        },
+        -- Hide the incline window when the cursorline is active
+        hide = {
+          cursorline = true,
+        },
+        -- Content rendering
+        render = function(props)
+          -- Get the filename
+          local filename =
+            vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename -- Indicate if the file is modified
+          end
+
+          -- Get the icon and color for the file
+          local icon, color =
+            require("nvim-web-devicons").get_icon_color(filename)
+
+          -- Return the content to render
+          return {
+            { icon, guifg = color },
+            { " " },
+            { filename },
+          }
+        end,
+      })
+    end,
+  },
+}
