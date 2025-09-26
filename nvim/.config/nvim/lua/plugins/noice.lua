28ce07919680290db96b6570d797bc38b9ceb151
D3us <victoriomolinabermejo@gmail.com>
2025-09-26 14:44:28 +0200

    feat(alerts, nvim): Customize notification alert timeout

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,7 +1,8 @@
 -- ~/.config/nvim/lua/plugins/noice.lua - Folke's noice.nvim config
--- Last updated: September 25, 2025
+-- Last updated: September 26, 2025
 
 return {
+  -- Noice
   {
     "folke/noice.nvim",
     opts = function(_, opts)
@@ -13,5 +14,12 @@ return {
         opts = { skip = true },
       })
     end,
+  },
+  -- Notification timeout
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 7500,
+    },
   },
 }
